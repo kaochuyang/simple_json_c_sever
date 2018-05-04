8d7915d706ffaba4578e73f773f68b5f17c7791c
kaochuyang <gordon209.tem97@g2.nctu.edu.tw>
2018-05-04 21:36:08 +0800

    test c++ to java

--- main.cpp
+++ main.cpp
@@ -17,25 +17,31 @@ using namespace std;
 int main()
 {
 
-Json::Value root;
-Json::FastWriter fast_writer;
-root["REGION_ID"]="600901";
+    Json::Value root;
+    Json::FastWriter fast_writer;
+    root["REGION_ID"]="600901";
+
+     root["REGION_ID5"]="700901";
+      root["REGION_ID8"]="6009055";
+       root["REGION_ID10"]="6009";
 
 
 
-
+    string A=//"{ 123456 }";
+        root.toStyledString().c_str();
 
     int listenfd,connfd;
     struct sockaddr_in servaddr;
     char buff[4096];
+    char buff2[4096];
     int n;
 
     //?«Ø¤@?TCPªºsocket
-    if( (listenfd = socket(AF_INET,SOCK_STREAM,0)) == -1) {
+    if( (listenfd = socket(AF_INET,SOCK_STREAM,0)) == -1)
+    {
         printf(" create socket error: %s (errno :%d)\n",strerror(errno),errno);
         return 0;
     }
-
 
     //¥ý§â¦a§}²MªÅ¡A??¥ô·NIP
     memset(&servaddr,0,sizeof(servaddr));
@@ -43,35 +49,45 @@ root["REGION_ID"]="600901";
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(5000);
 
+
     //¦a§}?©w¨ìlistenfd
-    if ( bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1) {
+    if ( bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1)
+    {
         printf(" bind socket error: %s (errno :%d)\n",strerror(errno),errno);
         return 0;
     }
 
     //?§vlistenfd
-    if( listen(listenfd,10) == -1) {
+    if( listen(listenfd,10) == -1)
+    {
         printf(" listen socket error: %s (errno :%d)\n",strerror(errno),errno);
         return 0;
     }
 
-   while(1)
-    {
     printf("====waiting for client's request=======\n");
     //accept ©Mrecv,ª`·N±µ¦¬¦r²Å¦ê²K¥[?§ô²Å'\0'
+    while(1)
+    {
 
-/////////////////////block 1
-        if( (connfd = accept(listenfd, (struct sockaddr *)NULL, NULL))  == -1) {
+        if( (connfd = accept(listenfd, (struct sockaddr *)NULL, NULL))  == -1)
+        {
             printf(" accpt socket error: %s (errno :%d)\n",strerror(errno),errno);
             return 0;
         }
-
-        printf("hello\n");
         n = recv(connfd,buff,MAXLINE,0);
         buff[n] = '\0';
         printf("recv msg from client:%s\n",buff);
-    }    close(connfd);
 
+        if ( send(connfd,A.c_str(),A.size(),0) <0)
+        {
+            printf("send msg error: %s(errno :%d)\n",strerror(errno),errno);
+            return 0;
+        }
+printf("size=%d\n",A.size());
+
+        printf("%s",A.c_str());
+        close(connfd);
+    }
     close(listenfd);
 
 
